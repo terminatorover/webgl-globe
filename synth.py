030517a2a2d7496062a4ec0e14028af60e71abef
Ubuntu <ubuntu@ip-172-31-1-90.us-west-2.compute.internal>
2013-11-29 03:55:58 +0000

    formatting

--- synth.py
+++ synth.py
@@ -7,12 +7,13 @@ def format (line):
 
     s = line.split()
     ls = len(s)
-    country = s[:ls-4]
-    ten = s[-1]
-    five = s[-2]
-    zero = s[-3]
-    nine = s[-4]
-    return 
+    country = s[1:ls-4]
+    country = "".join(country)
+    ten = float(s[-1])
+    five = float(s[-2])
+    zero = float(s[-3])
+    nine = float(s[-4])
+    return [ country , nine, zero, five,ten]
 
 g = geocoders.GoogleV3()
 
@@ -25,7 +26,10 @@ location, (lat, lng) = g.geocode("Korea (Democratic People's Rep. of)" )
 print lat, lng
 
 for line in input.readlines():
+    '''
     k =  line.split()
     k = k[1: len(k)-4]
     k = "".join(k)
     print k 
+    '''
+    print format(line)
