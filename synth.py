8b47f75b52e84c1f809653893c29d22598b7d345
Ubuntu <ubuntu@ip-172-31-1-90.us-west-2.compute.internal>
2013-11-29 03:50:48 +0000

    input perfect

--- synth.py
+++ synth.py
@@ -5,6 +5,27 @@ input = open("data.txt","r+")
 
 def format (line):
 
+    s = line.split()
+    ls = len(s)
+    country = s[:ls-4]
+    ten = s[-1]
+    five = s[-2]
+    zero = s[-3]
+    nine = s[-4]
+    return 
+
+g = geocoders.GoogleV3()
+
+
+#location, (lat, lng) = g.geocode("Spain")
+#location, (lat, lng) = g.geocode("Hong Kong, China (SAR)")
+location, (lat, lng) = g.geocode("Korea (Democratic People's Rep. of)" )
+
+
+print lat, lng
 
 for line in input.readlines():
-    print line
+    k =  line.split()
+    k = k[1: len(k)-4]
+    k = "".join(k)
+    print k 
