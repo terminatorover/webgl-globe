6e7e3762b34b34eb6f22a3bc1cb55296e75c5dd7
Ubuntu <roberaetana@yahoo.com>
2013-12-18 22:35:11 +0000

    the coridinates file ,and thecode that puts it into a dictionary [country]==>[lat,lng] format for easy lookup

--- clean_up.py
+++ clean_up.py
@@ -1,15 +1,45 @@
 #/bin/usr/python 
 
-input_file = open("cia_cords","r+")
-
-for line in input_file.readlines():
-
-    s = line.split()
-    ls = len(s)
-    print s
-    print s[:-6]
-#    country = s[1:ls-4]
- #   country = " ".join(country)
-#    print " ".join(s[:-7]) + str(s[-6:]) + "\n"
+def my_format( cords):
+    '''given the input ['28', '12', 'N,', '177', '22', 'W']
+    return the lat and the long in the form 
+    [28,-177] note that the negative is to point out that its the WEST , and we ignore minutes since we are not 
+    looking for that level of accuracy, we need a coordinate representation for the whole country "
     
+    '''
+
+    lat = int(cords[0])
+    lng = int(cords[-3])
+    if ( cords[2]== "S"):
+        lat = lat*-1
+    if ( cords[-1] == "W"):
+        lng = lng * -1
+
+    return [lat,lng]
+
+
+def main():
+    input_file = open("cia_cords","r+")
+
+    country_cord ={}#this dictionary will have country:[lat,lng] as a key value pair
+
+    for line in input_file.readlines():
+        if (len(line) < 5 ):
+            continue 
+        s = line.split()
+        ls = len(s)
+        #print s
+        country =  s[:-6]
+#        print s[-6:]
+        country = " ".join(country)
     
+
+        country_cord[country]= my_format(s[-6:])
+        
+        
+    ##NOW WE HAVE A DICTIONARY FOR FINDING THE LAT AND LONG
+    #----Check 
+#    print country_cord
+    input_data = open(,"r+")
+    
+main()
